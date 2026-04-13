4dc8e08876244d3c9e345d3cb8ad53295bf5abf2
ac4os <ac4os.dev@gmail.com>
2026-04-13 17:05:21 -0300

    Inserção de dados e estrutura inicial

---
+++ Cinema.js
@@ -0,0 +1,30 @@
+use('cinema'); 
+
+// EXCLUSÃO DE COLEÇÕES 
+db.filmes.drop(); 
+db.sessoes.drop(); 
+
+print("==============================");
+print("BANCO DE DADOS: Cinema");
+print("COLEÇÕES: filmes, sessoes");
+print("==============================");
+
+// Filmes 
+db.filmes.insertMany([
+  { _id: 1, titulo: "Vingadores: Ultimato", ano: 2019, genero: "Ação", duracao: 181, nacional: false, classificacao: 12 },
+  { _id: 2, titulo: "O Auto da Compadecida", ano: 2000, genero: "Comédia", duracao: 104, nacional: true, classificacao: 12 },
+  { _id: 3, titulo: "Duna: Parte 2", ano: 2024, genero: "Ficção Científica", duracao: 166, nacional: false, classificacao: 14 },
+  { _id: 4, titulo: "Cidade de Deus", ano: 2002, genero: "Drama", duracao: 130, nacional: true, classificacao: 18 },
+  { _id: 5, titulo: "Interestelar", ano: 2014, genero: "Ficção Científica", duracao: 169, nacional: false, classificacao: 10 },
+  { _id: 6, titulo: "Bacurau", ano: 2019, genero: "Suspense", duracao: 131, nacional: true, classificacao: 16 },
+  { _id: 7, titulo: "O Poderoso Chefão", ano: 1972, genero: "Crime", duracao: 175, nacional: false, classificacao: 14 },
+  { _id: 8, titulo: "Minha Mãe é uma Peça", ano: 2013, genero: "Comédia", duracao: 84, nacional: true, classificacao: 12 },
+  { _id: 9, titulo: "Coringa", ano: 2019, genero: "Drama", duracao: 122, nacional: false, classificacao: 16 },
+  { _id: 10, titulo: "Tropa de Elite", ano: 2007, genero: "Ação", duracao: 115, nacional: true, classificacao: 16 },
+  { _id: 11, titulo: "Parasita", ano: 2019, genero: "Suspense", duracao: 132, nacional: false, classificacao: 16 },
+  { _id: 12, titulo: "Central do Brasil", ano: 1998, genero: "Drama", duracao: 113, nacional: true, classificacao: 12 },
+  { _id: 13, titulo: "Matrix", ano: 1999, genero: "Ficção Científica", duracao: 136, nacional: false, classificacao: 14 },
+  { _id: 14, titulo: "O Menino e o Mundo", ano: 2013, genero: "Animação", duracao: 80, nacional: true, classificacao: 0 },
+  { _id: 15, titulo: "Batman", ano: 2022, genero: "Ação", duracao: 176, nacional: false, classificacao: 14 }
+]);
+
